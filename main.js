e5219cee1bc0a2591058cc2c1370e07343052d20
tqk2811 <tqk2811@gmail.com>
2017-07-04 03:18:31 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
-
+//Change raw.githubusercontent.com to rawgit.com (non-production) or cdn.rawgit.com
+$.getScript("https://cdn.rawgit.com/tqk2811/travian_ajax/master/sidebarBoxLinklist.js");
+$.getScript("https://cdn.rawgit.com/tqk2811/travian_ajax/master/sidebarBoxActiveVillage.js");
