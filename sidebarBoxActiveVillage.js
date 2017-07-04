f3d0a7a64e1e68c57434e879ab7ff602aba7964e
tqk2811 <tqk2811@gmail.com>
2017-07-04 08:47:54 +0700

    Update sidebarBoxActiveVillage.js

--- sidebarBoxActiveVillage.js
+++ sidebarBoxActiveVillage.js
@@ -1,37 +1,31 @@
-var market = "/build.php?gid=17";
-var barracks = "/build.php?gid=19";
-var stable = "/build.php?gid=20";
-var workshop = "/build.php?gid=21";
-
 var sidebarBoxActiveVillage = document.getElementById("sidebarBoxActiveVillage");
 var innerBox_header_sidebarBoxActiveVillage = sidebarBoxActiveVillage.getElementsByClassName("innerBox header ")[0];
-var innerBox_header_sidebarBoxActiveVillage_script_arr = innerBox_header_sidebarBoxActiveVillage.getElementsByTagName("script");
+//var innerBox_header_sidebarBoxActiveVillage_script_arr = innerBox_header_sidebarBoxActiveVillage.getElementsByTagName("script");
+var list_sidebarBoxActiveVillage = [
+    [document.getElementsByClassName("layoutButton workshopBlack gold  ")[0],"/build.php?gid=21"],//workshop
+    [document.getElementsByClassName("layoutButton stableBlack gold  ")[0],"/build.php?gid=20"],//stable
+    [document.getElementsByClassName("layoutButton barracksBlack gold  ")[0],"/build.php?gid=19"],//barracks
+    [document.getElementsByClassName("layoutButton marketBlack gold  ")[0],"/build.php?gid=17"]//market
+]
 
-var workshop_bt = document.getElementsByClassName("layoutButton workshopBlack gold  ")[0];
-var stable_bt = document.getElementsByClassName("layoutButton stableBlack gold  ")[0];
-var barracks_bt = document.getElementsByClassName("layoutButton barracksBlack gold  ")[0];
-var market_bt = document.getElementsByClassName("layoutButton marketBlack gold  ")[0];
-
-function Change_sidebarBoxActiveVillage_Button(element_name,uri,val_ = -1)
+function Change_sidebarBoxActiveVillage_Button(index)
 {
-    if (element_name !== null && element_name !== undefined)
+    var item = list_sidebarBoxActiveVillage[index];
+    if (item[0] !== null && item[0] !== undefined)
     {
-        var attibute_class = element_name.getAttribute("class");
+        var attibute_class = item[0].getAttribute("class");
         if(attibute_class.search("disable") > 0) { return;}
-        element_name.setAttribute("class",attibute_class.replace("Black","White").replace("gold","green"));
-        element_name.setAttribute("onclick","Change_sidebarBoxActiveVillage_Button_onclick(\""+uri+"\")");
-        if( val_ >= 0)
-        { 
-            //console.log(innerBox_header_sidebarBoxActiveVillage_script_arr[val_].innerHTML);
-            //innerBox_header_sidebarBoxActiveVillage_script_arr[val_].remove();
-        }
+        item[0].setAttribute("class",attibute_class.replace("Black","White").replace("gold","green"));
+        item[0].setAttribute("onclick","Change_sidebarBoxActiveVillage_Button_onclick(\""+item[1]+"\")");
+        //console.log(innerBox_header_sidebarBoxActiveVillage_script_arr[index].innerHTML);
+        //innerBox_header_sidebarBoxActiveVillage_script_arr[index].remove();
     }
 }
 function Change_sidebarBoxActiveVillage_Button_onclick(uri)
 {
     window.location = uri;
 }
-Change_sidebarBoxActiveVillage_Button(workshop_bt,workshop,0);
-Change_sidebarBoxActiveVillage_Button(stable_bt,stable,1);
-Change_sidebarBoxActiveVillage_Button(barracks_bt,barracks,2);
-Change_sidebarBoxActiveVillage_Button(market_bt,market,3);
+for(var i = 0; i < list_sidebarBoxActiveVillage.length; i++)
+{
+    Change_sidebarBoxActiveVillage_Button(i);
+}
