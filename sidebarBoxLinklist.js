8d40d7ea45644bb8fd4fea798e77f805ad7ed473
tqk2811 <tqk2811@gmail.com>
2017-07-04 08:52:30 +0700

    Update sidebarBoxLinklist.js

--- sidebarBoxLinklist.js
+++ sidebarBoxLinklist.js
@@ -1,28 +1,6 @@
-Element.prototype.remove = function() 
-{
-    this.parentElement.removeChild(this);
-}
-NodeList.prototype.remove = HTMLCollection.prototype.remove=function()
-{
-    for(var i=this.length-1;i>= 0;i--)
-    {
-        if(this[i] && this[i].parentElement)
-        {
-            this[i].parentElement.removeChild(this[i]);
-        }
-    }
-}
-function AddLinkerList(item,parent)
-{
-    var li_ = document.createElement('li');
-    var aTag = document.createElement('a');
-    aTag.setAttribute('href',item[1]);
-    aTag.innerHTML = item[0];
-    li_.appendChild(aTag);
-    parent.appendChild(li_);
-}
 //[Name,url]
-var list = [
+var ul_linkerlist;
+var list_sidebarBoxLinklist = [
     ["FarmList","/build.php?tt=99&id=39"],
     ["Att Comming","/build.php?gid=16&tt=1&filter=1&subfilters=1"],
     ["Green Attack Report","/berichte.php?t=1&opt=AAABAA=="],
@@ -33,14 +11,23 @@ var sidebarBoxLinklist_ = document.getElementById("sidebarBoxLinklist");
 var BoxLinklist_InnerBox = sidebarBoxLinklist_.getElementsByClassName("sidebarBoxInnerBox")[0];
 var innerBox_content = BoxLinklist_InnerBox.getElementsByClassName("innerBox content")[0];
 var linklistNotice_ = innerBox_content.getElementsByClassName("linklistNotice");
+function AddLinkerList(item)
+{
+    var li_ = document.createElement('li');
+    var aTag = document.createElement('a');
+    aTag.setAttribute('href',item[1]);
+    aTag.innerHTML = item[0];
+    li_.appendChild(aTag);
+    ul_linkerlist.appendChild(li_);
+}
+
 if( linklistNotice_!== null)
 {
     linklistNotice_[0].remove();
-    var ul_linkerlist = document.createElement("ul");
+    ul_linkerlist = document.createElement("ul");
     innerBox_content.appendChild(ul_linkerlist);
-    
     for(var i = 0;i < list.length;i++)
     {
-        AddLinkerList(list[i],ul_linkerlist);
+        AddLinkerList(list_sidebarBoxLinklist[i]);
     }
 }
