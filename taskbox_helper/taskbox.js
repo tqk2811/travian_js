bd35d950f0c09753ea10093b9239106503adb8b7
tqk2811 <tqk2811@gmail.com>
2017-09-08 09:32:29 +0700

    Create taskbox.js

---
+++ taskbox_helper/taskbox.js
@@ -0,0 +1,29 @@
+var ALAMI = function(){
+  return{
+    Draggable: function(){}
+  };
+}();
+function elementDraggable(e){ var e = e || window.event;
+  var div = taskbox;//ALAMI.Draggable.elemen;
+  ALAMI.Draggable.innerX = e.clientX + window.pageXOffset - div.offsetLeft;
+  ALAMI.Draggable.innerY = e.clientY + window.pageYOffset - div.offsetTop;
+
+  window.addEventListener('mousemove', elementMove, false);
+  window.addEventListener('mouseup', function(){
+    window.removeEventListener('mousemove', elementMove, false);
+    }, true);
+
+  function elementMove(e){
+    div.style.position = 'absolute';
+    div.style.left = e.clientX + window.pageXOffset - ALAMI.Draggable.innerX + 'px';
+    div.style.top = e.clientY + window.pageYOffset - ALAMI.Draggable.innerY + 'px';
+  }}
+
+var taskbox_travianjs_ = '<table width="100%" border="1"><tbody><tr><td>Village</td><td>Wood</td><td>clay</td><td>Iron</td><td>Crop</td><td>Build timeleft</td></tr></tbody></table>';
+var b = document.getElementById("center");
+var taskbox = document.createElement('div');
+taskbox.innerHTML = taskbox_travianjs_;
+taskbox.setAttribute("id","taskbox_travianjs");
+taskbox.setAttribute("style","position: absolute; opacity: 1; z-index: 6010; left: 243px; top: 161px; background-color:#ffffff; border-radius: 25px");
+b.appendChild(taskbox);
+taskbox.addEventListener('mousedown', function(e){ALAMI.Draggable.elemen = e.target || e.srcElement; elementDraggable(e);}, false);
