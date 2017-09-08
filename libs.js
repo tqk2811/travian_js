aa0e1a48f33ff2de7d9c70fc600a842adb5d7abf
tqk2811 <tqk2811@gmail.com>
2017-09-08 08:58:22 +0700

    Update libs.js

--- libs.js
+++ libs.js
@@ -12,5 +12,5 @@ NodeList.prototype.remove = HTMLCollection.prototype.remove=function()
         }
     }
 }
-AddScript("7343b480/sidebarBoxActiveVillage.js");
-AddScript("7343b480/sidebarBoxLinklist.js");
+AddGithubScript("tqk2811","travian_js","master","sidebarBoxActiveVillage.js");
+AddGithubScript("tqk2811","travian_js","master","sidebarBoxLinklist.js");
