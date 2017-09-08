d41e3fc17574486415bf337a351fe545643efebe
tqk2811 <tqk2811@gmail.com>
2017-09-08 09:49:31 +0700

    Update taskbox.js

--- taskbox_helper/taskbox.js
+++ taskbox_helper/taskbox.js
@@ -17,13 +17,13 @@ function elementDraggable(e){
     div.style.position = 'absolute';
     div.style.left = e.clientX + window.pageXOffset - ALAMI.Draggable.innerX + 'px';
     div.style.top = e.clientY + window.pageYOffset - ALAMI.Draggable.innerY + 'px';
-    localstorage.setItem("taskbox_style",taskbox.getAttribute("style"));
+    localStorage.setItem("taskbox_style",taskbox.getAttribute("style"));
   }}
 
 var taskbox_travianjs_ = '<table width="100%" border="1"><tbody><tr><td>Village</td><td>Wood</td><td>clay</td><td>Iron</td><td>Crop</td><td>Build timeleft</td></tr></tbody></table>';
 taskbox.innerHTML = taskbox_travianjs_;
 taskbox.setAttribute("id","taskbox_travianjs");
-var taskbox_style = localstorage.getItem("taskbox_style");
+var taskbox_style = localStorage.getItem("taskbox_style");
 if(taskbox_style !== null) taskbox.setAttribute("style",taskbox_style);
 else taskbox.setAttribute("style","position: absolute; opacity: 1; z-index: 6010; left: 243px; top: 161px; background-color:#ffffff; border-radius: 25px");
 b.appendChild(taskbox);
