755d2972aa1109373ef8b261ab7ac10bc6e76659
tqk2811 <tqk2811@gmail.com>
2017-09-13 16:22:18 +0700

    Update sidebarBoxLinklist.js

--- travian_plus/sidebarBoxLinklist.js
+++ travian_plus/sidebarBoxLinklist.js
@@ -12,7 +12,7 @@ function AddLinkerList(item)
     li_.appendChild(aTag);
     ul_linkerlist.appendChild(li_);
 }
-if( linklistNotice_!== null | linklistNotice_!== undefine)
+if( linklistNotice_!== null | linklistNotice_!== undefined)
 {
     linklistNotice_[0].remove();
     ul_linkerlist = document.createElement("ul");
