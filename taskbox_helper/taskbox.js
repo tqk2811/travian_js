4a0e667fdf50d0a53aba75a9ce3fecbb4f6ef4c2
tqk2811 <tqk2811@gmail.com>
2017-09-13 19:49:37 +0700

    Update taskbox.js

--- taskbox_helper/taskbox.js
+++ taskbox_helper/taskbox.js
@@ -44,5 +44,11 @@ taskbox.addEventListener('mousedown', function(e){ALAMI.Draggable.elemen = e.tar
 //read list village
 var sidebarBoxVillagelist = document.getElementById("sidebarBoxVillagelist");
 var listVillage = sidebarBoxVillagelist.getElementsByTagName("li");
+//load data from localStorage
+var active_village = function(){
+  for(int i = 0; i < listVillage.length; i++) if(listVillage[i].getAttribute("class") === " active") return listVillage[i];
+  return null; }
+//refresh data from active village
+
 
 //Travian.TimersAndCounters.initTimer(element_timer);// <span class="timer" couting="down" value=222></span>
