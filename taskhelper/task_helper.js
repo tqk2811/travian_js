fa9413483df8b864ddca918e99e42f5ed2be6a4a
tqk2811 <tqk2811@gmail.com>
2017-09-16 10:37:55 +0700

    Update task_helper.js

--- taskhelper/task_helper.js
+++ taskhelper/task_helper.js
@@ -1,22 +1,72 @@
-//var village_object = {Storage=80000,Granary=80000,ID=2143
+//var village_object = {Storage=80000,Granary=80000,ID="2143"
 //                        Resource=[27352,12321,24343,20654],//Wood,Clay,Iron,Crop
 //                        Production=[2800,2800,2800,3000],
 //  var Tasks_TimeEnd=[2365664,3335544,23353354]};
+function getUrlVars(uri)
+{
+    var vars = [], hash;
+    var hashes = uri.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+function LoadLiResource(e,value,max)
+{
+  var res = document.createElement("div");
+  var percent = Math.round((value * 100)/max,0);
+  if(percent <10) res.innerText = "0"+Math.round((value * 100)/max,0) + "% ";
+  else res.innerText = Math.round((value * 100)/max,0) + "% ";
+  e.appendChild(e);
+}
 function LoadVillageData(li_element,village_data)
 {
+  var a = document.createElement("a");
+  li_element.appendChild(a);
+  LoadLiResource(a,village_data.Resource[0],village_data.Storage);
+  LoadLiResource(a,village_data.Resource[1],village_data.Storage);
+  LoadLiResource(a,village_data.Resource[2],village_data.Storage);
+  LoadLiResource(a,village_data.Resource[3],village_data.Granary);
+  
+  
+  
 }
 
-//list elements village
 var sidebarBoxVillagelist = document.getElementById("sidebarBoxVillagelist");
-var listVillage = sidebarBoxVillagelist.getElementsByTagName("li");
+var listVillage = sidebarBoxVillagelist.getElementsByTagName("li");//list elements village
 var active_village = function(){
   for(int i = 0; i < listVillage.length; i++) if(listVillage[i].getAttribute("class") === " active") return listVillage[i];
-  return null; }
-
-
-//refresh data active village
-
-
-
+  return null; };
+var villages_data = localStorage.getItem("villages_data");//load from localStorage
+for(int i =0; i < listVillage.length; i++)
+{
+  var id = getUrlVars(listVillage[i].getAttribute("href"))[newdid];
+  if(listVillage[i] === active_village)
+  {
+    //update data current village
+    var Wood = document.getElementById("11").innerText;
+    var Clay = document.getElementById("12").innerText;
+    var Iron = document.getElementById("13").innerText;
+    var Crop = document.getElementById("14").innerText;
+    var Storage = document.getElementById("stockBarWarehouse").innerText.replace(".","").replace(",","");
+    var Granary = document.getElementById("stockBarGranary").innerText.replace(".","").replace(",","");
+    var build = document.getElementsByClassName("buildDuration");
+    if(build !== null & build !== undefined)
+    {
+      var current_second1970 = Math.round(Date.now()/1000,0);
+      for(int k=0; k < build.length; k++)
+      {
+        var timeleft = build[k].getElementsByTagName("span")[0].getAttribute("value");
+        
+      }
+    }
+    villages_data[id] = {};
+    localStorage.setItem("villages_data",villages_data);
+  }
+  if(villages_data[id] !== null & villages_data[id] !== undefined) LoadVillageData(listVillage[i],villages_data[id]);    
+}
 
 //Travian.TimersAndCounters.initTimer(element_timer);// <span class="timer" couting="down" value=222></span>
