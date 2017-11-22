8ed225eccafc1eef3cf9ed92789eff8a7205ba25
tqk2811 <tqk2811@gmail.com>
2017-11-22 09:28:34 +0700

    Update travian_libs.js

--- travian_libs.js
+++ travian_libs.js
@@ -13,27 +13,13 @@ NodeList.prototype.remove = HTMLCollection.prototype.remove=function()
     }
 }
 function CCSStylesheetRuleStyle(stylesheet, selectorText, style, value){
-  /* returns the value of the element style of the rule in the stylesheet
-  *  If no value is given, reads the value
-  *  If value is given, the value is changed and returned
-  *  If '' (empty string) is given, erases the value.
-  *  The browser will apply the default one
-  *
-  * string stylesheet: part of the .css name to be recognized, e.g. 'default'
-  * string selectorText: css selector, e.g. '#myId', '.myClass', 'thead td'
-  * string style: camelCase element style, e.g. 'fontSize'
-  * string value optionnal : the new value
-  */
   var CCSstyle = undefined, rules;
   for(var m in document.styleSheets){
-    if(document.styleSheets[m].href !== null || document.styleSheets[m].href !== undefined)
+    if(document.styleSheets[m].href === stylesheet)
     {
-      if(document.styleSheets[m].href.indexOf(stylesheet) != -1)
-      {
-        rules = document.styleSheets[m][document.all ? 'rules' : 'cssRules'];
-        for(var n in rules){if(rules[n].selectorText == selectorText){CCSstyle = rules[n].style; break;}}
-        break;
-      }
+      rules = document.styleSheets[m][document.all ? 'rules' : 'cssRules'];
+      for(var n in rules){if(rules[n].selectorText == selectorText){CCSstyle = rules[n].style; break;}}
+      break;      
     }
   }
   if(value == undefined)
