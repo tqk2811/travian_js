1b68935d22ff38293039a99309370d74390791ed
tqk2811 <tqk2811@gmail.com>
2018-03-27 00:11:58 +0700

    Fix cost

--- other/cost.js
+++ other/cost.js
@@ -1,14 +1,17 @@
 var contract_ = document.getElementById("contract");
-if(contract_ !== null | contract_ !== undefined)
+if(contract_ !== null || contract_ !== undefined)
 {	
 	var ress = contract_.getElementsByTagName("span");
-	var total_ = 0;
-	for(var i =0; i < ress.length - 1; i++)
+	if(ress !== null || ress !== undefined)
 	{
-		total_ += Number.parseInt(ress[i].innerText);
+		var total_ = 0;
+		for(var i =0; i < 4; i++)
+		{
+			total_ += Number.parseInt(ress[i].innerText);
+		}
+		var parent_ress = ress[0].parentNode;
+		var total_element = document.createElement("span");
+		total_element.innerText = "Total: " + total_;
+		parent_ress.appendChild(total_element);
 	}
-	var parent_ress = ress[0].parentNode;
-	var total_element = document.createElement("span");
-	total_element.innerText = "Total: " + total_;
-	parent_ress.appendChild(total_element);
 }
