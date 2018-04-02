8405a6b8bc9406cb9f384323d5fa427f1c9fa956
tqk2811 <tqk2811@gmail.com>
2018-04-03 01:52:11 +0700

    fix

--- other/market.js
+++ other/market.js
@@ -23,7 +23,7 @@ if(Flag_deleteAll_Trading_routes)
 		var sels = trading_routes.getElementsByClassName("sel");
 		if(sels !== null && sels !==undefined && sels.length >0)
 		{
-			var a_tr = sels[i].getElementsByTagName("a");
+			var a_tr = sels[0].getElementsByTagName("a");
 			if(a_tr !== null && a_tr !== undefined && a_tr.length >0 )a_tr.click();
 		}
 	}
