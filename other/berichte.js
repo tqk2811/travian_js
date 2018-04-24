b495ad9cd40a5b5f7861eaeb694117e69381354d
tqk2811 <tqk2811@gmail.com>
2018-04-24 07:55:14 +0700

    clear report confirm

--- other/berichte.js
+++ other/berichte.js
@@ -13,9 +13,12 @@ function clear_report()
 
 function clear_report_onclick()
 {
-	berichte_input_sellect_all_1.click();
-	var e_del = document.getElementById("del");
-	if(e_del !== null) e_del.click();
+	if(window.confirm("Are you sure to clear reports?"))
+	{
+		berichte_input_sellect_all_1.click();
+		var e_del = document.getElementById("del");
+		if(e_del !== null) e_del.click();
+	}
 }
 
 function berichte_main()
