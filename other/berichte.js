80ac978371c1602870456c02467b843ea9420372
tqk2811 <tqk2811@gmail.com>
2018-04-24 08:01:34 +0700

    fix

--- other/berichte.js
+++ other/berichte.js
@@ -3,11 +3,11 @@ function clear_report()
 	var e_div = berichte_input_sellect_all_1.parentElement;
 	if(e_div !== null)
 	{
-		var e_div_clear = document.createElement("button");
-		e_div_clear.innerText = "check all and delete";
-		e_div_clear.setAttribute("style","background-color:green;border:none;color:white;padding:3px;margin:3px");
-		e_div_clear.setAttribute("onclick","clear_report_onclick()");
-		e_div.insertAdjacentElement("afterend",e_div_clear);
+		var e_clear = document.createElement("span");
+		e_clear.innerText = "check all and delete";
+		e_clear.setAttribute("style","background-color:green;border:none;color:white;padding:3px;margin:3px");
+		e_clear.setAttribute("onclick","clear_report_onclick()");
+		e_div.insertAdjacentElement("afterend",e_clear);
 	}	
 }
 
@@ -15,9 +15,9 @@ function clear_report_onclick()
 {
 	if(window.confirm("Are you sure to clear reports?"))
 	{
-		//berichte_input_sellect_all_1.click();
-		//var e_del = document.getElementById("del");
-		//if(e_del !== null) e_del.click();
+		berichte_input_sellect_all_1.click();
+		var e_del = document.getElementById("del");
+		if(e_del !== null) e_del.click();
 	}
 }
 
