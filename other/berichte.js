68bd845112fef8e368bd53a5525ffa53df947103
tqk2811 <tqk2811@gmail.com>
2018-04-27 23:29:35 +0700

    count res report

--- other/berichte.js
+++ other/berichte.js
@@ -1,4 +1,4 @@
-function clear_report()
+function berichte_clear_report()
 {
 	var e_div = berichte_input_sellect_all_1.parentElement;
 	if(e_div !== null)
@@ -6,12 +6,12 @@ function clear_report()
 		var e_clear = document.createElement("span");
 		e_clear.innerText = "check all and delete";
 		e_clear.setAttribute("style","background-color:green;border:none;color:white;padding:3px;margin:3px");
-		e_clear.setAttribute("onclick","clear_report_onclick()");
+		e_clear.setAttribute("onclick","berichte_clear_report_onclick()");
 		e_div.insertAdjacentElement("afterend",e_clear);
 	}	
 }
 
-function clear_report_onclick()
+function berichte_clear_report_onclick()
 {
 	if(window.confirm("Are you sure to clear reports?"))
 	{
@@ -21,12 +21,28 @@ function clear_report_onclick()
 	}
 }
 
+function berichte_count_res()
+{
+	var rAreas = document.getElementClassName("rArea");
+	if(rAreas.length >= 4)
+	{
+		var count = 0;
+		for(var i =0; i <4; i++) count += Number.parseInt(rAreas[i].innerText);
+		
+		var e_total  = document.createElement("span");
+		e_total.innerText = "(Total:" + count + ")";
+		var e_parent_rArea = rAreas[0].parentElement;
+		e_parent_rArea.appendChild(e_total);
+	}
+}
+
 function berichte_main()
 {
 	if(window.location.href.indexOf("berichte.php")>=0)
 	{
 		berichte_input_sellect_all_1 = document.getElementById("sAll1");
-		clear_report();
+		if(berichte_input_sellect_all_1 !== null) berichte_clear_report();
+		if(window.location.href.indexOf("id=") >=0) berichte_count_res();
 	}
 }
 
