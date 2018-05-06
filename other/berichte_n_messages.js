0198ca0fb94e6d7130d756e9b2639f089d7c7f9c
tqk2811 <tqk2811@gmail.com>
2018-05-07 00:12:02 +0700

    swap

--- other/berichte_n_messages.js
+++ other/berichte_n_messages.js
@@ -3,17 +3,17 @@ function berichte_clear_report()
 	var e_div = berichte_input_sellect_all_1.parentElement;
 	if(e_div !== null)
 	{
-		var e_read = document.createElement("span");
-		e_read.innerText = "Mask as read all";
-		e_read.setAttribute("style","background-color:green;border:none;color:white;padding:3px;margin:3px");
-		e_read.setAttribute("onclick","berichte_read_report_onclick()");
-		e_div.insertAdjacentElement("afterend",e_read);
-		
 		var e_clear = document.createElement("span");
 		e_clear.innerText = "Clear all";
 		e_clear.setAttribute("style","background-color:green;border:none;color:white;padding:3px;margin:3px");
 		e_clear.setAttribute("onclick","berichte_clear_report_onclick()");
 		e_div.insertAdjacentElement("afterend",e_clear);
+		
+		var e_read = document.createElement("span");
+		e_read.innerText = "Mask as read all";
+		e_read.setAttribute("style","background-color:green;border:none;color:white;padding:3px;margin:3px");
+		e_read.setAttribute("onclick","berichte_read_report_onclick()");
+		e_div.insertAdjacentElement("afterend",e_read);		
 	}	
 }
 
