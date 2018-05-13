a4224bcc158669152b73738b18ade934632b2c2c
tqk2811 <tqk2811@gmail.com>
2018-05-13 23:37:17 +0700

    fix

--- other/allianz.js
+++ other/allianz.js
@@ -1,19 +1,17 @@
 function ally_roa_attackcount()
 {
 	var allianceMembers = document.getElementsByClassName("allianceMembers");
-	console.log("debug allianceMembers");
 	if(allianceMembers.length == 1)
 	{
 		var attacks = allianceMembers[0].getElementsByClassName("attack");
 		for(var  i = 0; i < attacks.length; i++ )
 		{
 			var counts= attacks[i].getAttribute("alt").split(" ");
-			if(counts >= 1)
+			if(counts.length >= 1)
 			{
 				var e_numattack = document.createElement("a1");
 				e_numattack.innerText = "(" + counts[0] + ")";
 				attacks[i].insertAdjacentElement("beforebegin",e_numattack);
-				console.log("insert success");
 			}
 		}
 	}
