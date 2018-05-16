ef67ec12096a6b71d7710b063c739a037204e1e0
tqk2811 <tqk2811@gmail.com>
2018-05-16 23:40:17 +0700

    update

--- travian_plus/sidebarBoxActiveVillage.js
+++ travian_plus/sidebarBoxActiveVillage.js
@@ -7,21 +7,20 @@ function Change_sidebarBoxActiveVillage_Button(index)
 		if(attibute_class.search("disable") > 0) { return;}
 		item[0].setAttribute("class",attibute_class.replace("Black","White").replace("gold","green"));
 		item[0].setAttribute("onclick","Change_sidebarBoxActiveVillage_Button_onclick(\""+item[1]+"\")");
+		$("#"+item[0].id).off("click");
 	}
 }
 function Change_sidebarBoxActiveVillage_Button_onclick(uri){window.location = uri;}
 
+var list_sidebarBoxActiveVillage = [
+	[document.getElementsByClassName("layoutButton workshopBlack gold  ")[0],"/build.php?gid=21"],//workshop
+	[document.getElementsByClassName("layoutButton stableBlack gold  ")[0],"/build.php?gid=20"],//stable
+	[document.getElementsByClassName("layoutButton barracksBlack gold  ")[0],"/build.php?gid=19"],//barracks
+	[document.getElementsByClassName("layoutButton marketBlack gold  ")[0],"/build.php?gid=17"]//market
+]
 var sidebarBoxActiveVillage = document.getElementById("sidebarBoxActiveVillage");
 if(sidebarBoxActiveVillage !== null)
 {
-	var list_sidebarBoxActiveVillage = [
-		[document.getElementsByClassName("layoutButton workshopBlack gold  ")[0],"/build.php?gid=21"],//workshop
-		[document.getElementsByClassName("layoutButton stableBlack gold  ")[0],"/build.php?gid=20"],//stable
-		[document.getElementsByClassName("layoutButton barracksBlack gold  ")[0],"/build.php?gid=19"],//barracks
-		[document.getElementsByClassName("layoutButton marketBlack gold  ")[0],"/build.php?gid=17"]//market
-	]
 	var innerBox_header_sidebarBoxActiveVillage = sidebarBoxActiveVillage.getElementsByClassName("innerBox header ")[0];
-	//var scripts_remove = sidebarBoxActiveVillage.getElementsByTagName("script");
-	//for(var i = 0; i < scripts_remove.length; ) scripts_remove[0].remove();
 	for(var i = 0; i < list_sidebarBoxActiveVillage.length; i++) Change_sidebarBoxActiveVillage_Button(i);
 }
