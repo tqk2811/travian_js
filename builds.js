1f100dfa7812a6b8d32ea93cc343d152709d9bcd
tqk2811 <tqk2811@gmail.com>
2018-06-22 10:48:16 +0700

    test

--- builds.js
+++ builds.js
@@ -39,28 +39,25 @@ function gid16()
 			e_bt_CheckNonAttacking.innerText = "Check non attacking";
 			e_bt_CheckNonAttacking.setAttribute("onclick","gid16_bt_CheckNonAttacking_onclick(false)");
 
-			window.e_cb_check_yellow = document.createElement("input");
-			e_cb_check_yellow.setAttribute("type","checkbox");
-			e_cb_check_yellow.setAttribute("id","e_cb_check_yellow");	
-			window.e_cb_check_red = document.createElement("input");
-			e_cb_check_red.setAttribute("type","checkbox");
-			e_cb_check_red.setAttribute("id","e_cb_check_red");
+			window.gid16_cb_yellow = document.createElement("input");
+			gid16_cb_yellow.setAttribute("type","checkbox");
+			var e_LB_yellow = document.createElement("label");
+			e_LB_yellow.innerText = "Yellow";
+			e_LB_yellow.setAttribute("style","border:none;color:white;padding: 3px; margin: 3px;");
 			
-						
+			window.gid16_cb_red = document.createElement("input");
+			gid16_cb_red.setAttribute("type","checkbox");
+			var e_LB_red = document.createElement("label");
+			e_LB_red.innerText = "Red";
+			e_LB_red.setAttribute("style","border:none;color:white;padding: 3px; margin: 3px;");			
+			
 			var e_div = document.createElement("div");
 			e_div.appendChild(e_bt_CheckNonAttacking);
-			e_div.appendChild(e_cb_check_yellow);
-			e_div.appendChild(e_cb_check_red);
+			e_div.appendChild(window.gid16_cb_yellow);
+			e_div.appendChild(e_LB_yellow);
+			e_div.appendChild(window.gid16_cb_red);
+			e_div.appendChild(e_LB_red);
 			e_build.insertAdjacentElement("afterbegin",e_div);
-			
-			var e_LB_yellow = document.createElement("label");
-			e_LB_yellow.setAttribute("for","e_cb_check_yellow");
-			e_LB_yellow.innerText = "Yellow";
-			e_LB_yellow.setAttribute("style","border:none;color:white;padding: 3px; margin: 3px;");			
-			var e_LB_red = document.createElement("label");
-			e_LB_red.setAttribute("for","e_cb_check_red");
-			e_LB_red.innerText = "Red";
-			e_LB_red.setAttribute("style","border:none;color:white;padding: 3px; margin: 3px;");
 		}
 		else if(tabItem.getAttribute("href").indexOf("tt=2")>=0)
 		{
