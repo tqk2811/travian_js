7f8d65d0f1799145fe33e4480bb5e52d0a5d7353
tqk2811 <tqk2811@gmail.com>
2018-06-25 12:03:17 +0700

    update

--- other/spieler.js
+++ other/spieler.js
@@ -12,10 +12,32 @@ function spieler_addraidlist()
 		e_sellect.appendChild(e_option);
 	}
 	var villages = document.getElementById("villages");
-	if(villages !== null) villages.insertAdjacentElement("beforebegin",e_sellect);
-	
-	
-	
+	if(villages !== null) 
+	{
+		villages.insertAdjacentElement("beforebegin",e_sellect);
+		
+		var e_th = document.createElement("th");
+		e_th.width = "20px";
+		e_th.innerText = "Add FarmList";
+		villages.getElementsByTagName("thead")[0].getElementsByTagName("tr")[0].insertAdjacentElement("beforeend",e_th);
+		
+		var e_coords = villages.getElementsByClassName("coords");
+		for(var i =0; i < e_coords.length; i++)
+		{
+			var uri_ = e_coords[i].getElementsByTagName("a")[0].href;
+			var x_ = getParameterByName("x",uri_);
+			var y_ = getParameterByName("y",uri_);
+						
+			var e_button = document.createElement("button");
+			e_button.innerText = "Add";
+			e_button.setAttribute("style","background-color:green;border:none;color:white;");
+			e_button.setAttribute("onclick","Travian.Game.RaidList.addSlotPopupWrapper(e_sellect.value.toString(), '"+x_"', '"+y_"'); return false;");
+			
+			var e_td = document.createElement("td");
+			e_td.appendChild(e_button);
+			e_coords[i].insertAdjacentElement("afterend",e_td);
+		}
+	}
 }
 
 
