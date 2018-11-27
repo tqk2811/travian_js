71cf32ceb360ae268073de9c1cb5c801624134cb
tqk2811 <tqk2811@gmail.com>
2018-11-27 08:49:09 +0700

    Update berichte_n_messages.js

--- other/berichte_n_messages.js
+++ other/berichte_n_messages.js
@@ -55,10 +55,10 @@ function berichte_count_res()
 			if(canraid_c > 0) canraid += canraid_c;
 		}		
 		var e_total  = document.createElement("span");
-		if(rAreas.length == 1) e_total.innerText = "(Can Raid/Total: " + canraid + "/"  + total + ")";
-		else e_total.innerText = "(Total: " + total + ")";		
-		e_total.setAttribute("style","margin-left: 10px");
-		var e_parent = entityWrapper_resource[0].parentElement;
+		if(rAreas.length == 1) e_total.innerText = "Can Raid/Total: " + canraid + "/"  + total;
+		else e_total.innerText = "Total: " + total;		
+		//e_total.setAttribute("style","margin-left: 10px");
+		var e_parent = entityWrapper_resource[0].parentElement.parentElement;
 		e_parent.insertAdjacentElement("afterend",e_total);
 	}
 }
