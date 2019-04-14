e8235fc59a72ce242c8196903e1291dd61b19a4f
tqk2811 <tqk2811@gmail.com>
2019-04-15 02:40:23 +0700

    Update npc_helper.js

--- src/npc_helper.js
+++ src/npc_helper.js
@@ -105,10 +105,9 @@ window.npc_helper = {
 	mouseUp :function(){window.removeEventListener('mousemove', this.divMove, true);},
 	mouseDown :function(e){window.addEventListener('mousemove', this.divMove, true);},
 	divMove :function(e){
-		var div = document.getElementById('dxy');
-		div.style.position = 'absolute';
-		div.style.top = e.clientY + 'px';
-		div.style.left = e.clientX + 'px';
+		this.e_div.style.position = 'absolute';
+		this.e_div.style.top = e.clientY + 'px';
+		this.e_div.style.left = e.clientX + 'px';
 	},​
 	
 	Trade : function(){
