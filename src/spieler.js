5b7c6939920913e7cfac03b21ebc3dd2e09ec43a
tqk2811 <tqk2811@gmail.com>
2019-05-09 00:16:02 +0700

    Update spieler.js

--- src/spieler.js
+++ src/spieler.js
@@ -105,7 +105,7 @@ function func_hero_code()
 	var td = document.createElement("td");
 	var a_ = document.createElement("a");
 	a_.setAttribute("href","/statistiken.php?id=3&name=" + spieler_uid);
-	a_.innerText = "Find hero";
+	a_.innerText = "Find hero exp";
 	if(detected_item) td.innerText = detected;
 	th.appendChild(a_);
 	tr.appendChild(th);
