d57887945b21c52d98776f9be8d8e7cbc6e57086
tqk2811 <tqk2811@gmail.com>
2019-06-30 00:10:15 +0700

    Update libs.js

--- libs.js
+++ libs.js
@@ -156,7 +156,7 @@ TJS = {
 		var f = marketPlace.sendRessources;
 		window.marketPlace.sendRessources = function(){
 			var thisArg = document.getElementById(id);
-			f.apply(thisArg);
+			f();//.apply(thisArg);
 			callback_();
 		}
 	},
