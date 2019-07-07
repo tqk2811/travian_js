4edf2d407f44064becaf1501595dfe6d8a5704ba
tqk2811 <tqk2811@gmail.com>
2019-07-07 10:55:22 +0700

    fix

--- libs.js
+++ libs.js
@@ -442,7 +442,7 @@ TJS.Const = {
 		"c_2" : { r: [14850,16625,16000,3350], run_twice: 2},//Big Celebration / 2
 		"c_3" : { r: [9900,11084,10667,2234], run_twice: 3}//Big Celebration / 3
 	},
-	imgs_troop_move = [	["def1","/build.php?gid=16&tt=1&filter=1&subfilters=2,3"],//all def in
+	imgs_troop_move : [	["def1","/build.php?gid=16&tt=1&filter=1&subfilters=2,3"],//all def in
 						["def2","/build.php?gid=16&tt=1&filter=2&subfilters=5"],//def yellow out
 						["def3","/build.php?gid=16&tt=1&filter=1&subfilters=2,3"],//all def in
 						["att1","/build.php?gid=16&tt=1&filter=1&subfilters=1"],//att red in
