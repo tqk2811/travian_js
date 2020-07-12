917f7fde7eef53448e80c3fdce093b1be010d700
tqk2811 <tqk2811@gmail.com>
2020-07-12 19:24:07 +0700

    Update builds.js

--- src/builds.js
+++ src/builds.js
@@ -249,20 +249,6 @@ function gid16_raidall_init()
 		TJS.SaveCurrentAccount();
 	}
 }
-function gid16_bt_raidallInterval()
-{
-	var loading_hides = listEntry.getElementsByClassName("loading hide");
-	if(loading_hides.length == 1)
-	{
-		var _switchClosed = listEntry.getElementsByClassName("switchClosed");//expanded
-		if(_switchClosed.length == 0)
-		{
-			window.clearInterval(window.gid16_raidall_Intervalhandle);
-			gid16_bt_CheckGreen_onclick(listid);//click raid
-		}
-		else _switchClosed[0].click();
-	}
-}
 
 
 function gid16_bt_CheckGreen_onclick(listid){
@@ -275,7 +261,20 @@ function gid16_bt_CheckGreen_onclick(listid){
 		switchClosed[0].click();
 		if(window.gid16_flagraidall)
 		{
-			window.gid16_raidall_Intervalhandle = window.setInterval(gid16_bt_raidallInterval,500);
+			window.gid16_raidall_Intervalhandle = window.setInterval(function()
+			{
+				var loading_hides = listEntry.getElementsByClassName("loading hide");
+				if(loading_hides.length == 1)
+				{
+					var _switchClosed = listEntry.getElementsByClassName("switchClosed");//expanded
+					if(_switchClosed.length == 0)
+					{
+						window.clearInterval(window.gid16_raidall_Intervalhandle);
+						gid16_bt_CheckGreen_onclick(listid);//click raid
+					}
+					else _switchClosed[0].click();
+				}
+			},500);
 		}
 		return;
 	}
